287f1a534d02d8cf3c90a8573ff2d0d21f901e65
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2018-01-14 12:17:38 -0300

    Modified main to read until EOF

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,15 @@ int main() {
     MinorDevices *others = new MinorDevices();
     Datapath datapath(registerBank, control, aluControl, alu, mem, others);
 
-    // vector<bit> addi = {
-    //     // addi $t0, $zero, 5
-    //     0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1
-    // };
+    string instruction;
 
-    string addi = "00100000000010000000000000000101";
-
-    datapath.setInstruction(addi);
-    datapath.processInstruction();
-    datapath.showRegData();
-    datapath.showALUData();
-    datapath.showMainMemData();
+    while (cin << instruction) {
+        datapath.setInstruction(instruction);
+        datapath.processInstruction();
+        datapath.showRegData();
+        datapath.showALUData();
+        datapath.showMainMemData();
+    }
 
     return 0;
 }
